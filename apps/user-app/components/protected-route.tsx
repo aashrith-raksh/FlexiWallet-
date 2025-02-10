d570fc083dfa6fc6f746ef9b6e5c009ea66178cd
Aashrith Raksh <aashrith.raksh@gmail.com>
2025-02-10 10:11:18 +0530

    fix: fix bugs in protection logic

--- apps/user-app/components/protected-route.tsx
+++ apps/user-app/components/protected-route.tsx
@@ -1,15 +1,25 @@
-"use client";
-import { redirect } from "next/navigation";
-
-import { ReactNode } from "react";
+"use client"
 import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
-export default function ProtectedRoute({ children }: { children: ReactNode }) {
-  const { data: session } = useSession();
+export default function ProtectedRoute({ children }: { children: React.ReactNode }) {
+  const { data: session, status } = useSession();
+  const router = useRouter();
 
-  if (!session?.user) {
-    return redirect("/api/auth/signin");
-  } else {
+  useEffect(() => {
+    if (status === "unauthenticated") {
+      router.push("/api/auth/signin"); // Redirect to sign-in page
+    }
+  }, [status, router]);
+
+  if (status === "loading") {
+    return <p>Loading...</p>; // Show a loader while checking session
+  }
+
+  if (session) {
     return <>{children}</>;
   }
+
+  return null; // This prevents flickering of protected content
 }
